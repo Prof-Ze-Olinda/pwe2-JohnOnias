b6b23cbab8bb9e68352d4185e8eea9c055216ff6
JohnOnias <john.emanuel.onias07@aluno.ifce.edu.br>
2025-10-02 16:32:31 -0300

    respotas atividade

--- js/script.js
+++ js/script.js
@@ -4,18 +4,21 @@ const botao = document.getElementById("startBtn");
 
 botao.addEventListener("click", () => {
     // TODO: peça ao usuário seu nome usando prompt()
-
+    let user_name = prompt('digite seu nome!'); 
+    
     // TODO: peça o ano de nascimento e armazene em anoNascimento
-
+    let anoNascimento = prompt("digite o seu ano de nascimento!");
     // Use uma variável para o ano atual
+    let anoAtual = 2025;
 
     // TODO: converta o ano de nascimento para número (parseInt)
-
+    anoNascimento = parseInt(anoNascimento); 
     // TODO: exiba com alert() uma frase: "Olá, NOME! Você tem X anos."
-
+    let user_age = anoAtual - anoNascimento;
+    alert("Olá, " + user_name + " Você tem  " + user_age + "  anos");
     // TODO: use confirm() para perguntar se a idade está correta
-
+    let user_confirm = confirm("A idade esta certa?");
     // Exiba no console a escolha do usuário (console.log ou console.warn)
-
+    console.log(user_confirm);
     // Melhore o programa adicionando outros recursos
 });
